bfec8f7753d824037a291a32f0f6192938903456
chemzqm <chemzqm@gmail.com>
2015-11-06 01:14:41 +0800

    rework

--- example/example.js
+++ example/example.js
@@ -1,57 +1,61 @@
-var Sortable = require('..');
+var Sortable = require('..')
 
-var els = document.querySelectorAll('.numbers, .languages, .handle');
-var ul = document.querySelector('.one');
+var els = document.querySelectorAll('.numbers, .languages, .handle')
+var ul = document.querySelector('.one')
 
 // all
-[].slice.call(els).forEach(function(el){
-  var sortable = new Sortable(el);
-  if ('handle' == el.className) sortable.handle('span');
-  sortable.ignore('[disabled]');
-  sortable.bind('li');
-});
+;[].slice.call(els).forEach(function(el){
+  var sortable = new Sortable(el)
+  if ('handle' == el.className) sortable.handle('span')
+  sortable.ignore('[disabled]')
+  sortable.bind('li')
+})
 
-var publicEl = document.querySelector('.public');
-var s = new Sortable(publicEl);
-s.max(5);
-s.bind('li');
-s.on('max', function(count) {
-  alert('max count ' + count +' reached');
-});
+var publicEl = document.querySelector('.public')
+var s = new Sortable(publicEl)
+s.max(5)
+s.bind('li')
+s.on('update', function(el){
+  console.log(el)
+})
+//s.on('max', function(count) {
+//  alert('max count ' + count +' reached')
+//})
 
-var privateEl = document.querySelector('.private');
-var s1 = new Sortable(privateEl);
-s1.bind('li');
-s.connect(s1);
+var privateEl = document.querySelector('.private')
+var s1 = new Sortable(privateEl)
+s1.bind('li')
+s.connect(s1)
+//s1.connect(s)
 s1.on('update', function(el){
-    console.log(el);
-});
+  console.log(el)
+})
 
-var p = document.querySelector('#languages');
+var p = document.querySelector('#languages')
 document.getElementById('add').addEventListener('click', function(){
-    var n = document.createElement('div');
-    n.innerHTML = '<li>Python</li>';
-    p.appendChild(n);
-}, false);
+    var n = document.createElement('li')
+    n.innerHTML = 'Python'
+    p.appendChild(n)
+}, false)
 
 document.getElementById('remove').addEventListener('click', function(){
-    var n = p.firstElementChild;
-    p.removeChild(n);
-}, false);
+    var n = p.firstElementChild
+    p.removeChild(n)
+}, false)
 
-(function(){
-  var p1 = document.querySelector('.p1');
-  var s1 = new Sortable(p1);
-  s1.bind('li');
+;(function(){
+  var p1 = document.querySelector('.p1')
+  var s1 = new Sortable(p1)
+  s1.bind('li')
   s1.on('update', function(el){
-      console.log(el);
-  });
-  var p2 = document.querySelector('.p2');
-  var s2 = new Sortable(p2);
-  s2.bind('li');
-  s1.connect(s2);
-  s2.connect(s1);
+      console.log(el)
+  })
+  var p2 = document.querySelector('.p2')
+  var s2 = new Sortable(p2)
+  s2.bind('li')
+  s1.connect(s2)
+  s2.connect(s1)
   s2.on('update', function(el){
-      console.log(el);
-  });
+      console.log(el)
+  })
 })()
